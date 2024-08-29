7c9b2efce52a229d4e732ba7cfbb3aec7a11adf1
Padzx <padzxgabriel@gmail.com>
2024-08-29 03:00:44 -0300

    Add missing configuration files and main application class

---
+++ log-ingestion/src/main/java/org/example/LogIngestionApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class LogIngestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogIngestionApplication.class, args);
+    }
+}
