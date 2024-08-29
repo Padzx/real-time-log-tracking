fbf6a76071fc0cd0547ea633f69eb35ce98c389c
Padzx <padzxgabriel@gmail.com>
2024-08-29 02:36:05 -0300

    Configure Gradle build scripts, module build files, wrapper, and dependencies

---
+++ log-ingestion/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.3.3"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "org.example"
+version = "unspecified"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+
+    testImplementation("org.springframework.boot:spring-boot-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
