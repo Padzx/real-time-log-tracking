5a1ceaa06639e1cd0a2d81b9e1b3a43af89c30c8
Padzx <padzxgabriel@gmail.com>
2024-08-29 02:18:46 -0300

    Configure Gradle build scripts and dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("org.springframework.boot") version "3.3.3" apply false
+    id("io.spring.dependency-management") version "1.1.6" apply false
+    id("java")
+}
+
+group = "org.example"
+version = "1.0.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter")
+        testImplementation(platform("org.junit:junit-bom:5.10.0"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+
+rootProject.name = "real-time-log-tracking"
+
+// modules
+include("log-ingestion")
