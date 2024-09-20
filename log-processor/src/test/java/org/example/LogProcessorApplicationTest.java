c67a1406dd75afab0cc7836345d17ea150000270
Padzx <padzxgabriel@gmail.com>
2024-09-20 13:38:22 -0300

    log processor application test class for the testing context

--- log-processor/src/test/java/org/example/LogProcessorApplicationTest.java
+++ log-processor/src/test/java/org/example/LogProcessorApplicationTest.java
@@ -3,12 +3,14 @@ package org.example;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.example.service.LogProcessingService;
+import org.example.repository.LogProcessingRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
@@ -18,6 +20,9 @@ public class LogProcessorApplicationTest {
 
     @Autowired
     private LogProcessingService logProcessingService;
+
+    @MockBean
+    private LogProcessingRepository logProcessingRepository;
 
     @Test
     void contextLoads() {
