36c2761a435fa9c80ca68384ef156810f9ff2ff3
Padzx <padzxgabriel@gmail.com>
2024-10-03 17:02:44 -0300

    [UPDATED] - start.sh script for initialization docker compose

--- docker/start.sh
+++ docker/start.sh
@@ -1,55 +1,66 @@
 #!/bin/bash
-# sh script required for docker compose startup
-# with required kafka and zookeeper directories
+# sh script required for docker compose startup with Kafka and Zookeeper
 
 ZOOKEEPER_DATA_DIR="./zookeeper/data"
 ZOOKEEPER_LOG_DIR="./zookeeper/data/log"
 ZOOKEEPER_EXTERNAL_LOG_DIR="/var/log/zookeeper"
 KAFKA_DATA_DIR="./kafka/data"
+PERMISSION_LEVEL=775
+PERMISSION_OWNER=1000:1000
 
-# Create internal Zookeeper directories if they don't exist
+check_error() {
+  if [ $? -ne 0 ]; then
+    echo "Error executing the previous command. Ending the script."
+    exit 1
+  fi
+}
+
 if [ ! -d "$ZOOKEEPER_DATA_DIR" ]; then
     echo "Creating Zookeeper data directory..."
     mkdir -p "$ZOOKEEPER_DATA_DIR"
+    check_error
 fi
 
-if [ ! -d "$ZOOKEEPER_LOG_DIR" ]; then
-    echo "Creating Zookeeper log directory..."
+if [ ! -d "$ZOOKEEPER_LOG_DIR/version-2" ]; then
+    echo "Creating Zookeeper log directory (version-2)..."
     mkdir -p "$ZOOKEEPER_LOG_DIR/version-2"
+    check_error
 fi
 
-# Set permissions for Zookeeper directories
-echo "Setting permissions for Zookeeper directories..."
-sudo chmod -R 777 "$ZOOKEEPER_DATA_DIR"
-sudo chmod -R 777 "$ZOOKEEPER_LOG_DIR"
+echo "Configuring permissions for Zookeeper directories..."
+chmod -R $PERMISSION_LEVEL "$ZOOKEEPER_DATA_DIR"
+check_error
+chmod -R $PERMISSION_LEVEL "$ZOOKEEPER_LOG_DIR"
+check_error
 
-# Create external log directory for Zookeeper logs
 if [ ! -d "$ZOOKEEPER_EXTERNAL_LOG_DIR" ]; then
     echo "Creating external Zookeeper log directory..."
-    sudo mkdir -p "$ZOOKEEPER_EXTERNAL_LOG_DIR"
+    mkdir -p "$ZOOKEEPER_EXTERNAL_LOG_DIR"
+    check_error
 fi
+chmod -R $PERMISSION_LEVEL "$ZOOKEEPER_EXTERNAL_LOG_DIR"
+check_error
 
-# Set permissions for the external log directory
-sudo chmod -R 777 "$ZOOKEEPER_EXTERNAL_LOG_DIR"
-
-# Ensure Kafka has permissions to write to its data directory
 if [ -d "$KAFKA_DATA_DIR" ]; then
-    echo "Setting ownership and permissions for Kafka data directory..."
-    sudo chown -R 1000:1000 "$KAFKA_DATA_DIR"
-    sudo chmod -R 775 "$KAFKA_DATA_DIR"
+    echo "Configuring Kafka Data Directory Permissions and Ownership..."
 else
     echo "Kafka data directory does not exist, creating now..."
     mkdir -p "$KAFKA_DATA_DIR"
-    sudo chown -R 1000:1000 "$KAFKA_DATA_DIR"
-    sudo chmod -R 775 "$KAFKA_DATA_DIR"
+    check_error
 fi
 
-# Clean up old containers and volumes
-echo "Cleaning up old containers and volumes..."
-docker compose down -v --remove-orphans
+chown -R $PERMISSION_OWNER "$KAFKA_DATA_DIR"
+check_error
+chmod -R $PERMISSION_LEVEL "$KAFKA_DATA_DIR"
+check_error
 
-# Start Docker containers
+echo "Cleaning old containers and volumes..."
+docker compose down -v --remove-orphans
+check_error
+
 echo "Starting Docker containers with Docker Compose..."
 docker compose up --build
+check_error
 
 echo "Docker environment configured and containers started successfully."
+cle
